e5973cb44ecf3621c28deea04624faa00d4848b5
paulscott56 <pscott209@gmail.com>
2016-11-19 15:00:54 +0000

    added in dict and status code

--- mobaping.py
+++ mobaping.py
@@ -3,18 +3,16 @@ import requests
 
 def ping_services(event, context):
     url_list = {
-        'nova' : 'https://novaservice?wsdl',
-        'payments' : 'https://PaymentWebService?wsdl',
-        'transfers' : 'https://transfersService?WSDL'
+        "payments" : "https://autoeuropepayments.novacarhire.com:8443/Mobacar/services/PaymentWebService?wsdl"
     }
-    url_list.update()
-    
-    for u in url_list:
+    url_list.update(event)
+
+    for key, value in url_list.iteritems():
         headers = {'content-type': 'text/xml'}
         # We set verify to false to avoid using a cert... Not good, but meh
-        response = requests.get(url, headers=headers, verify=False, allow_redirects=False, timeout=5) 
+        response = requests.get(value, headers=headers, verify=False, allow_redirects=False, timeout=5) 
         # log to CloudWatch
         response.raise_for_status()
-        #print response.text
+        print response.status_code
 
 
